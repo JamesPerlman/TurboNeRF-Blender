a368bf9cab34dc6b8ba7505f7b269e6c9315c3d8
JamesPerlman <jam.e.perl@gmail.com>
2022-09-24 22:07:39 -0700

    change defaults

--- photogrammetry_importer/importers/camera_importer.py
+++ photogrammetry_importer/importers/camera_importer.py
@@ -90,13 +90,13 @@ class CameraImporter:
         name="Add a Background Image for each Camera",
         description="The background image is only visible by viewing the "
         "scene from a specific camera",
-        default=True,
+        default=False,
     )
     add_image_planes: BoolProperty(
         name="Add an Image Plane for each Camera",
         description="Add an Image Plane for each Camera - only for "
         "non-panoramic cameras",
-        default=False,
+        default=True,
     )
     add_image_plane_emission: BoolProperty(
         name="Add Image Plane Color Emission",
