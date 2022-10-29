bf5e133b536a1b42ca0d6019ab2160b00859cf78
JamesPerlman <jam.e.perl@gmail.com>
2022-10-28 23:57:29 -0700

    fix get_active_camera (only get true active camera)

--- blender_utility/nerf_render_manager.py
+++ blender_utility/nerf_render_manager.py
@@ -1,7 +1,10 @@
 import bpy
 
 from blender_nerf_tools.blender_utility.obj_type_utility import get_nerf_obj_type
-from blender_nerf_tools.constants import OBJ_TYPE_RENDER_CAMERA
+from blender_nerf_tools.constants import (
+    OBJ_TYPE_RENDER_CAMERA,
+    RENDER_CAM_IS_ACTIVE_ID,
+)
 
 class NeRFRenderManager:
     @classmethod
@@ -14,5 +17,5 @@ class NeRFRenderManager:
     
     @classmethod
     def get_active_camera(cls):
-        render_cameras = cls.get_all_cameras()
+        render_cameras = [cam for cam in cls.get_all_cameras() if bool(cam[RENDER_CAM_IS_ACTIVE_ID]) is True]
         return render_cameras[0] if len(render_cameras) > 0 else None
