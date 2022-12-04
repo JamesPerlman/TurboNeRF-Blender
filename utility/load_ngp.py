d407cbe6ee831288fd667db055ff1c68af6dbeab
JamesPerlman <jam.e.perl@gmail.com>
2022-12-04 13:04:20 -0800

    fix pyd_dirs

--- utility/load_ngp.py
+++ utility/load_ngp.py
@@ -4,6 +4,8 @@ import sys
 NGP_DIR = "C:\\Users\\bizon\\Developer\\blender-ngp"
 
 pyd_dirs = []
+pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "build*", "**/*.pyd"), recursive=True)]
+pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "build*", "**/*.so"), recursive=True)]
 pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "out/build/x64-Debug*", "**/*.pyd"), recursive=True)]
 pyd_dirs += [os.path.dirname(pyd) for pyd in glob.iglob(os.path.join(NGP_DIR, "out/build/x64-Debug*", "**/*.so"), recursive=True)]
 
