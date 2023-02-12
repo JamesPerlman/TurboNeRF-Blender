610a3038d7edf03c472579667db0aa8afb2fc5f6
JamesPerlman <jam.e.perl@gmail.com>
2023-02-11 20:39:11 -0800

    add dotdict

---
+++ utility/dotdict.py
@@ -0,0 +1,7 @@
+# thank you https://stackoverflow.com/a/23689767/892990
+
+class dotdict(dict):
+    """dot.notation access to dictionary attributes"""
+    __getattr__ = dict.get
+    __setattr__ = dict.__setitem__
+    __delattr__ = dict.__delitem__
