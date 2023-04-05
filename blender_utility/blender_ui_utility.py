53c729d57d4e37a8fdfd64d2829e5987b21903c3
JamesPerlman <jam.e.perl@gmail.com>
2023-04-05 00:39:48 -0700

    set display device to none

--- blender_utility/blender_ui_utility.py
+++ blender_utility/blender_ui_utility.py
@@ -3,8 +3,12 @@ import bpy
 def switch_to_turbo_nerf_renderer(context: bpy.types.Context):
     context.space_data.shading.type = 'RENDERED'
     context.scene.render.engine = 'TURBO_NERF_RENDERER'
+    context.scene.display_settings.display_device = 'None'
+
 
 def is_turbo_nerf_renderer_active(context: bpy.types.Context):
     shading_type_is_rendered = context.space_data.shading.type == 'RENDERED'
     render_engine_is_turbo_nerf = context.scene.render.engine == 'TURBO_NERF_RENDERER'
-    return shading_type_is_rendered and render_engine_is_turbo_nerf
+    is_display_device_none = context.scene.display_settings.display_device == 'None'
+
+    return shading_type_is_rendered and render_engine_is_turbo_nerf and is_display_device_none
