2717fdfad5b9474242c57f0ee5d848f96f85d7eb
JamesPerlman <jam.e.perl@gmail.com>
2023-07-04 13:19:39 -0700

    add poll fn for reset training operator

--- panels/nerf_panel_operators/reset_nerf_training_operator.py
+++ panels/nerf_panel_operators/reset_nerf_training_operator.py
@@ -7,6 +7,13 @@ from turbo_nerf.utility.nerf_manager import NeRFManager
 class ResetNeRFTrainingOperator(bpy.types.Operator):
     bl_idname = "turbo_nerf.reset_nerf_training"
     bl_label = "Reset NeRF Training"
+
+    @classmethod
+    def poll(cls, context):
+        nerf_obj = get_active_nerf_obj(context)
+        nerf = NeRFManager.get_nerf_for_obj(nerf_obj)
+        
+        return nerf.training_step > 0
     
     def execute(self, context):
         NeRFManager.reset_training()
