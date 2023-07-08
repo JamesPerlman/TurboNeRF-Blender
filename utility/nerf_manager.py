1b7fc0662c0572452ee644cbb2a602dabe6926e5
JamesPerlman <jam.e.perl@gmail.com>
2023-07-08 16:39:54 -0700

    back to 0.0.15

--- utility/nerf_manager.py
+++ utility/nerf_manager.py
@@ -18,7 +18,7 @@ class NeRFManager():
     
     @classmethod
     def required_pylib_version(cls):
-        return "0.0.14"
+        return "0.0.15"
 
     @classmethod
     def is_pylib_compatible(cls):
