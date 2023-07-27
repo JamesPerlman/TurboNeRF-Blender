91e860a1a9f05d531d7292a19e8d25b17f794e4e
JamesPerlman <jam.e.perl@gmail.com>
2023-07-26 17:14:25 -0700

    version bump

--- utility/nerf_manager.py
+++ utility/nerf_manager.py
@@ -18,7 +18,7 @@ class NeRFManager():
     
     @classmethod
     def required_pylib_version(cls):
-        return "0.0.18"
+        return "0.0.19"
 
     @classmethod
     def is_pylib_compatible(cls):
